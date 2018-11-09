2f623a97849b24768d38b937eaa22ddbcc29a234
shuhei.morioka <selmertsx@gmail.com>
2018-11-09 12:04:12 +0900

    npm i -D tslint

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,13 @@ async function datadog_handler(): Promise<void> {
   const fromTime = moment({ hour: 0, minute: 0, second: 0 })
     .add(-1, "days")
     .format("X");
-
   const toTime = moment({ hour: 23, minute: 59, second: 59 }).format("X");
 
   for (const product of products) {
     const productMetrics: ProductMetrics = await datadogClient.countHosts(
       product,
       fromTime,
-      toTime,
+      toTime
     );
     attachments.push(SlackMessage.attachments(productMetrics));
   }
