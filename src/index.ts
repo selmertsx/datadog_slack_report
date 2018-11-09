f98eb14b4a5b861934c711e3ee1f0091a398d6fe
shuhei.morioka <selmertsx@gmail.com>
2018-11-09 11:54:49 +0900

    npm update

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,16 @@ const products: string[] = [PRODUCT];
 
 async function datadog_handler(): Promise<void> {
   const fromTime = moment({ hour: 0, minute: 0, second: 0 })
-    .add("days", -1)
+    .add(-1, "days")
     .format("X");
+
   const toTime = moment({ hour: 23, minute: 59, second: 59 }).format("X");
 
   for (const product of products) {
     const productMetrics: ProductMetrics = await datadogClient.countHosts(
       product,
       fromTime,
-      toTime
+      toTime,
     );
     attachments.push(SlackMessage.attachments(productMetrics));
   }
