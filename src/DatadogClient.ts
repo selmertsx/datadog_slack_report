fdb37e39fd1ae41802f26da72a01785f04b8b6ee
shuhei.morioka <selmertsx@gmail.com>
2018-11-09 12:10:36 +0900

    update tslint rules to ignore interface-name

--- src/DatadogClient.ts
+++ src/DatadogClient.ts
@@ -6,22 +6,26 @@ const APP_KEY: string = process.env.APP_KEY as string;
 const API_KEY: string = process.env.API_KEY as string;
 
 export class DatadogClient {
-  request: AxiosInstance;
+  public request: AxiosInstance;
 
   constructor() {
     this.request = axios.create({
       baseURL: "https://api.datadoghq.com/api/v1/",
-      timeout: 1000000
+      timeout: 1000000,
     });
   }
 
-  public async countHosts(product: string, from: string, to: string): Promise<ProductMetrics> {
+  public async countHosts(
+    product: string,
+    from: string,
+    to: string,
+  ): Promise<ProductMetrics> {
     const params: CountHostRequest = {
-      from,
-      to,
-      query: `count_not_null(sum:system.cpu.user{product:${product}} by {host})`,
+      api_key: API_KEY,
       application_key: APP_KEY,
-      api_key: API_KEY
+      from,
+      query: `count_not_null(sum:system.cpu.user{product:${product}} by {host})`,
+      to,
     };
 
     const res = await this.request.get("/query", { params });
