c2398b6d429b7f85fc2c9452e3bf6f214003da64
shuhei.morioka <selmertsx@gmail.com>
2019-01-01 01:37:22 +0900

    prettier SlackMessage.spec.ts

--- __test__/SlackMessage.spec.ts
+++ __test__/SlackMessage.spec.ts
@@ -1,37 +1,53 @@
-import { SlackMessage } from '../src/SlackMessage';
-import { ProductMetrics } from '../src/ProductMetrics';
-import { DatadogHostMetrics } from '../src/datadog';
+import { DatadogHostMetrics } from "../src/datadog";
+import { ProductMetrics } from "../src/ProductMetrics";
+import { SlackMessage } from "../src/SlackMessage";
 
 import moment from "moment";
 
-const firstTime = moment({years: 2018, months: 11, days: 18, hours: 9, minutes: 0, seconds: 0}).format("x");
-const lastTime = moment({years: 2018, months: 11, days: 18, hours: 10, minutes: 0, seconds: 0}).format("x");
+const firstTime = moment({
+  years: 2018,
+  months: 11,
+  days: 18,
+  hours: 9,
+  minutes: 0,
+  seconds: 0,
+}).format("x");
+
+const lastTime = moment({
+  years: 2018,
+  months: 11,
+  days: 18,
+  hours: 10,
+  minutes: 0,
+  seconds: 0,
+}).format("x");
+
 const maxHostCount = 40;
 const minHostCount = 20;
 const metrics: DatadogHostMetrics = {
-  product: "sample",
   pointlists: [
     {
-      unixTime: parseInt(firstTime),
       count: minHostCount,
+      unixTime: parseInt(firstTime, 10),
     },
     {
-      unixTime: parseInt(lastTime),
       count: maxHostCount,
-    }
-  ]
-}
+      unixTime: parseInt(lastTime, 10),
+    },
+  ],
+  product: "sample",
+};
 
 const title = metrics.product;
 const productMetrics = new ProductMetrics(metrics);
 const text: string =
-  `min:${productMetrics.minHostCount()} ~ max:${productMetrics.maxHostCount()}\n`
-  + `sum(host*hours):${productMetrics.sum()}`;
+  `min:${productMetrics.minHostCount()} ~ max:${productMetrics.maxHostCount()}\n` +
+  `sum(host*hours):${productMetrics.sum()}`;
 
 describe("attachments", () => {
   it("normal condition", () => {
-    const message = SlackMessage.attachments(productMetrics)
-    expect(message.title).toEqual(title)
+    const message = SlackMessage.attachments(productMetrics);
+    expect(message.title).toEqual(title);
     expect(message.text).toEqual(text);
   });
 });
