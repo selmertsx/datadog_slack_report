b9a4c3a98ef1393f0dd974dd4f6193240287d029
shuhei.morioka <selmertsx@gmail.com>
2019-01-23 20:26:59 +0900

    気の迷いでやっていた _xxx というプロパティのネーミングをやっぱり辞めることにした

--- src/ProductMetrics.ts
+++ src/ProductMetrics.ts
@@ -1,41 +1,33 @@
 import { DatadogHostMetrics, PointList } from "./datadog";
 
 export class ProductMetrics {
-  private _metrics: PointList[];
-  private _name: string;
-  private _counts: number[] | undefined;
+  public metrics: PointList[];
+  public name: string;
+  private counts: number[] | undefined;
 
   constructor(metrics: DatadogHostMetrics) {
-    this._name = metrics.product;
-    this._metrics = metrics.pointlists;
-    this._counts = undefined;
+    this.name = metrics.product;
+    this.metrics = metrics.pointlists;
+    this.counts = undefined;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  get metrics() {
-    return this._metrics;
-  }
-
-  get counts(): number[] {
-    if (!this._counts) {
-      this._counts = this.metrics.map((metric: PointList) => metric.count);
+  get hostCounts(): number[] {
+    if (!this.counts) {
+      this.counts = this.metrics.map((metric: PointList) => metric.count);
     }
 
-    return this._counts;
+    return this.counts;
   }
 
   public maxHostCount(): number {
-    return Math.max(...this.counts);
+    return Math.max(...this.hostCounts);
   }
 
   public minHostCount(): number {
-    return Math.min(...this.counts);
+    return Math.min(...this.hostCounts);
   }
 
   public sum(): number {
-    return this.counts.reduce((total, num) => total + num);
+    return this.hostCounts.reduce((total, num) => total + num);
   }
 }
