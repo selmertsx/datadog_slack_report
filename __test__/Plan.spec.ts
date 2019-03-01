7b89316fb7fdc4ef8270f0da7706307feed3f22a
shuhei.morioka <selmertsx@gmail.com>
2019-03-01 20:03:22 +0900

    Planは必要になるまでコメントアウトしておく

--- __test__/Plan.spec.ts
+++ __test__/Plan.spec.ts
@@ -3,7 +3,7 @@ import { Plan } from "../src/Plan";
 const plan = new Plan();
 
 describe("getHostCount", () => {
-  it("normal condition", () => {
+  xit("normal condition", () => {
     const response = plan.getHostCount();
     const expectResponse = { productA: 2, productB: 3 };
     expect(response).toEqual(expectResponse);
