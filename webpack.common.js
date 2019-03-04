dcaa3625a1b423269a52231c50259f1220f56fc8
shuhei.morioka <selmertsx@gmail.com>
2019-03-04 14:54:01 +0900

    remove babel settings

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,6 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: [
-          { loader: 'babel-loader' },
           { loader: 'ts-loader' },
         ],
         exclude: /node_modules/
