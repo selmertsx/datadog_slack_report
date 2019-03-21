85069e0c14d25b25d8503a88fac5d859a7741f64
shuhei.morioka <selmertsx@gmail.com>
2019-03-21 19:31:45 +0900

    PlanクラスをReservedPlanという名前に変えた。事前に抑えなければならない計画であるということを明示するために

--- src/ReservedPlan.ts
+++ src/ReservedPlan.ts
@@ -5,7 +5,7 @@ const projectId = process.env.PROJECT_ID;
  * 監視したいプロダクト、ホスト数を保存するDatastoreにアクセスする
  * 雑にPlanという名前にした。もう少し解像度が上がったら、そのときに適切な名前をつける
  */
-export class Plan {
+export class ReservedPlan {
   // 完全に動作確認のためのコード
   public async getHostCount() {
     const datastore = new Datastore({ projectId });
@@ -23,7 +23,7 @@ export class Plan {
   }
 }
 
-const plan = new Plan();
+const plan = new ReservedPlan();
 
 plan
   .getHostCount()
