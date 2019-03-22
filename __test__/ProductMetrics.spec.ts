d8ede0b64e470a5d9251040d93c4596be50e699f
shuhei.morioka <selmertsx@gmail.com>
2019-03-22 16:37:28 +0900

    ProductMetricsのテストコードについて、テストデータ作成部分を少しリファクタリングした

--- __test__/ProductMetrics.spec.ts
+++ __test__/ProductMetrics.spec.ts
@@ -3,23 +3,9 @@ import { DatadogHostMetrics } from "../src/datadog";
 import { ProductMetrics } from "../src/ProductMetrics";
 
 // tslint:disable: object-literal-sort-keys
-const firstTime = moment({
-  years: 2018,
-  months: 11,
-  days: 18,
-  hours: 9,
-  minutes: 0,
-  seconds: 0,
-}).format("x");
-const lastTime = moment({
-  years: 2018,
-  months: 11,
-  days: 18,
-  hours: 10,
-  minutes: 0,
-  seconds: 0,
-}).format("x");
-/* tslint:enable:object-literal-sort-keys */
+const firstTime = moment({ years: 2018, months: 11, days: 18, hours: 9, minutes: 0, seconds: 0 }).format("x");
+const lastTime = moment({ years: 2018, months: 11, days: 18, hours: 10, minutes: 0, seconds: 0 }).format("x");
+// tslint:enable:object-literal-sort-keys
 
 const maxHostCount = 40;
 const minHostCount = 20;
