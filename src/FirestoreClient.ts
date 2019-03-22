968cf237cac612862d3a577dd354f824f32602e1
shuhei.morioka <selmertsx@gmail.com>
2019-03-22 11:17:40 +0900

    ReservedPlanクラスはfirestoreにアクセスするだけのClassにすることにしたのでFirestoreClientという名前になった

--- src/FirestoreClient.ts
+++ src/FirestoreClient.ts
@@ -4,12 +4,12 @@ import { Firestore } from "@google-cloud/firestore";
  * 監視したいプロダクト、ホスト数を保存するDatastoreにアクセスする
  * 雑にPlanという名前にした。もう少し解像度が上がったら、そのときに適切な名前をつける
  */
-export class ReservedPlan {
+export class FirestoreClient {
   // 完全に動作確認のためのコード
   public async getHostCount() {
     const firestore = new Firestore();
-    const doc = firestore.doc("datadog/plan");
-    return await doc.get();
+    const docs = await firestore.collection("datadog").get();
+    docs.forEach(doc => {});
   }
 
   public async postHostCount(productName: string, hostNumber: number) {
@@ -22,9 +22,9 @@ export class ReservedPlan {
   }
 }
 
-const plan = new ReservedPlan();
+const client = new FirestoreClient();
 
-plan
+client
   .getHostCount()
   .then(console.log)
   .catch(console.error);
