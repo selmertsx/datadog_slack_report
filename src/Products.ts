232e16920d4523f7884fc677582345e2e728fcb6
shuhei.morioka <selmertsx@gmail.com>
2019-03-28 13:38:28 +0900

    overPeriodの途中

--- src/Products.ts
+++ src/Products.ts
@@ -1,4 +1,4 @@
-import { DatadogHostMetrics, ReservedPlan } from "./datadog";
+import { DatadogHostMetrics, PointList, ReservedPlan } from "./datadog";
 import { Product } from "./Product";
 
 export class Products {
@@ -16,6 +16,12 @@ export class Products {
   public list: Map<string, Product> = new Map();
 
   public overPeriod() {
-    return [];
+    const products: Product[] = Array.from(this.list.values());
+    const totalDesiredHostCount = products.map(product => product.desiredHostCount).reduce((sum, num) => sum + num);
+    const totalMetrics = products.map(product => product.metrics);
+    totalMetrics.map((metrics: PointList[]) => {
+      metrics.
+    });
+    return products;
   }
 }
