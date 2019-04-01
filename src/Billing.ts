9e8f7e9c7be45c82246c7e727312f06baabd8a42
shuhei.morioka <selmertsx@gmail.com>
2019-04-01 18:59:39 +0900

    add the comment that is the developing plan of Billing class

--- src/Billing.ts
+++ src/Billing.ts
@@ -1,7 +1,20 @@
 import { BillingReport } from "./BillingReport";
 
 export class Billing {
-  public additionalCollection(fromTime: number, toTime: number): BillingReport[] {
+  /**
+   * DatadogからfromTime, toTimeの期間内でデータを引っ張ってくる
+   * DataStoreからfromTime, toTimeの期間内での監視計画を取ってくる
+   * Datadogで計測したホスト数 <= DataStoreから取得した計測予定のホスト数の場合、計画した請求金額をslackに通知する。
+   * Datadogで計測したホスト数 > DataStoreから取得した計測予定のホスト数の場合、追加請求分を計算する。
+   * 請求金額の計算ロジックは DatadogBillingRulesとかそういうクラスを作ってやらせる。
+   * responseはBillingReportの配列であり、それらBillingReportの配列には下記の要素が含まれている
+   * BillingReport Object { name: "プロダクト名", plan: "予定していた監視ホスト数とその金額", additional: "予定していた分を超過した場合の追加徴収金額とその内訳を記載する" }
+   *
+   * @param fromTime
+   * @param toTime
+   */
+
+  public calculate(fromTime: number, toTime: number): BillingReport[] {
     return [new BillingReport("sample")];
   }
 }
