5cdbef77bd4c7c236c881a51e4b0af850a305baf
shuhei.morioka <selmertsx@gmail.com>
2019-04-01 17:46:04 +0900

    create base of Billing class

--- src/Billing.ts
+++ src/Billing.ts
@@ -1,1 +1,7 @@
-export class Billing {}
+import { BillingReport } from "./BillingReport";
+
+export class Billing {
+  public additionalCollection(fromTime: number, toTime: number): BillingReport[] {
+    return [new BillingReport("sample")];
+  }
+}
---
+++ src/BillingReport.ts
@@ -0,0 +1,3 @@
+export class BillingReport {
+  constructor(public name: string) {}
+}
