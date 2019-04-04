647ee33bfbedd51ef99d3306ff418fb23aa2effd
shuhei.morioka <selmertsx@gmail.com>
2019-04-05 00:30:10 +0900

    一旦今使っていないコードは削除する

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import moment from "moment-timezone";
 import "source-map-support/register";
 import { Billing } from "./Billing";
-import { DatadogHostMetrics, ReservedPlan } from "./datadog";
-import { DatadogClient } from "./DatadogClient";
-import { FirestoreClient } from "./FirestoreClient";
 import { SlackClient } from "./SlackClient";
-import { slackMessageBlock } from "./SlackMessageBlock";
 
-const datadogClient = new DatadogClient();
 const slackClient = new SlackClient();
 
 export async function datadog_handler(): Promise<void> {
@@ -24,37 +19,4 @@ export async function datadog_handler(): Promise<void> {
   const billing = new Billing();
   const report = await billing.calculate(fromTime, toTime);
   await slackClient.post(report.slackMessageDetail());
-}
-
-// cloud functionsの HTTP RequestがPATCHに対応してたらファンクション名変える
-export async function update_reserved_plan(req: any, res: any): Promise<void> {
-  const client = new FirestoreClient();
-  try {
-    await client.updateReservedPlan("sample", 40);
-    return res.status(200).send("ok");
-  } catch (err) {
-    return res.status(500).send(err);
-  }
-}
-
-// cloud functionsの HTTP RequestがPOSTに対応してたらファンクション名変える
-export async function create_reserved_plan(req: any, res: any): Promise<void> {
-  const client = new FirestoreClient();
-  try {
-    await client.postReservedPlan("sample", 40);
-    return res.status(200).send("ok");
-  } catch (err) {
-    return res.status(500).send(err);
-  }
-}
-
-// cloud functionsの HTTP RequestがDELETEに対応してたらファンクション名変える
-export async function delete_reserved_plan(req: any, res: any): Promise<void> {
-  const client = new FirestoreClient();
-  try {
-    await client.deleteReservedPlan("sample");
-    return res.status(200).send("ok");
-  } catch (err) {
-    return res.status(500).send(err);
-  }
 }
