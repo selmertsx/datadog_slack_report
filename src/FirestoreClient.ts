5324cd402fc17f97d6514726617d4263b8782a48
shuhei.morioka <selmertsx@gmail.com>
2019-04-05 15:31:45 +0900

    remove @google-cloud/firestore

--- src/FirestoreClient.ts
+++ src/FirestoreClient.ts
@@ -1,4 +1,8 @@
-import { Firestore, WriteResult } from "@google-cloud/firestore";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
 import { ReservedPlan } from "./datadog";
 
 /**
@@ -6,33 +10,13 @@ import { ReservedPlan } from "./datadog";
  * 雑にPlanという名前にした。もう少し解像度が上がったら、そのときに適切な名前をつける
  */
 export class FirestoreClient {
-  private firestore: Firestore;
-  constructor() {
-    this.firestore = new Firestore();
-  }
-
   public async getReservedPlans(): Promise<ReservedPlan[]> {
-    const docs = await this.firestore.collection("datadog").get();
+    const docs = await firestore.collection("datadog").get();
     const results: ReservedPlan[] = [];
     docs.forEach(doc => {
       const data = doc.data();
       results.push({ productName: data.name, plannedHostCount: data.host_number });
     });
     return results;
-  }
-
-  public async postReservedPlan(productName: string, plannedHostCount: number): Promise<WriteResult> {
-    const doc = this.firestore.collection("datadog").doc(productName);
-    return doc.create({ name: productName, host_number: plannedHostCount });
-  }
-
-  public async updateReservedPlan(productName: string, plannedHostCount: number): Promise<WriteResult> {
-    const doc = this.firestore.collection("datadog").doc(productName);
-    return doc.set({ host_number: plannedHostCount });
-  }
-
-  public async deleteReservedPlan(planName: string) {
-    const doc = this.firestore.collection("datadog").doc(planName);
-    return doc.delete();
   }
 }
