ca5cfdef928346c705fad38528fc557b3c043df4
shuhei.morioka <selmertsx@gmail.com>
2019-04-08 13:06:07 +0900

    add slack arguments types

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import moment from "moment-timezone";
 import "source-map-support/register";
 import { Billing } from "./Billing";
 import { SlackClient } from "./SlackClient";
+import { APIGatewayProxyResult } from "aws-lambda";
 
 const slackClient = new SlackClient();
 
-export async function datadog_handler(event, context) {
+export async function datadog_handler(event: AWSLambda.APIGatewayEvent, context: APIGatewayProxyResult) {
   const fromTime = moment({ hour: 0, minute: 0, second: 0 })
     .tz("Asia/Tokyo")
     .subtract(1, "days")
