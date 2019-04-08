92a61922f8b1f5f4f37d76cc3b16f31b5d49890f
shuhei.morioka <selmertsx@gmail.com>
2019-04-08 13:42:58 +0900

    datadog_handlerの引数を変えることにより、とりあえず動くところまで持ってきた。

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { WebAPICallResult } from "@slack/client";
+import { APIGatewayEvent, Callback, Context } from "aws-lambda";
 import moment from "moment-timezone";
 import "source-map-support/register";
 import { Billing } from "./Billing";
 import { SlackClient } from "./SlackClient";
-import { APIGatewayProxyResult } from "aws-lambda";
 
 const slackClient = new SlackClient();
 
-export async function datadog_handler(event: AWSLambda.APIGatewayEvent, context: APIGatewayProxyResult) {
+export async function datadog_handler(event: APIGatewayEvent, context: Context, callback: Callback) {
   const fromTime = moment({ hour: 0, minute: 0, second: 0 })
     .tz("Asia/Tokyo")
     .subtract(1, "days")
@@ -20,8 +20,19 @@ export async function datadog_handler(event: AWSLambda.APIGatewayEvent, context:
 
   const billing = new Billing();
   try {
-    const report = await billing.calculate(fromTime, toTime);
-    return console.log(report);
+    // const report = await billing.calculate(fromTime, toTime);
+    const result = {
+      status: 200,
+      message: "OK!!!!!",
+    };
+
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify(result),
+    });
   } catch (err) {
     throw new Error(err);
   }
