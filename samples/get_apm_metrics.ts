43c5ea374139b0adcb66ae73df39cb2416cf4896
shuhei.morioka <selmertsx@gmail.com>
2019-05-01 23:54:54 +0900

    apm数をカウントすることはできた

---
+++ samples/get_apm_metrics.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosInstance } from "axios";
+import moment from "moment-timezone";
+
+const APP_KEY: string = process.env.APP_KEY as string;
+const API_KEY: string = process.env.API_KEY as string;
+
+interface RequestParams {
+  baseURL: string;
+  timeout: number;
+}
+
+const requestParams: RequestParams = {
+  baseURL: "https://api.datadoghq.com/api/v1/",
+  timeout: 1000000,
+};
+
+const fromTime = moment({ hour: 0, minute: 0, second: 0 })
+  .tz("Asia/Tokyo")
+  .subtract(1, "days")
+  .format("X");
+
+const toTime = moment({ hour: 23, minute: 59, second: 59 })
+  .tz("Asia/Tokyo")
+  .subtract(1, "days")
+  .format("X");
+
+const getRequestParams = {
+  api_key: API_KEY,
+  application_key: APP_KEY,
+  query: "count_not_null(datadog.apm.host_instance{*} by {host}.rollup(count, 3600))",
+};
+
+export class DatadogClient {
+  private request: AxiosInstance;
+
+  constructor() {
+    this.request = axios.create(requestParams);
+  }
+
+  public async countHosts(from: string, to: string): Promise<any> {
+    const params = { ...getRequestParams, from, to };
+    const res = await this.request.get("/query", { params });
+
+    return res.data.series.map((productsMetrics: any) => {
+      const pointlists = new Map<number, number>();
+      console.log(productsMetrics);
+    });
+  }
+}
+
+(async () => {
+  const client = new DatadogClient();
+  await client.countHosts(fromTime, toTime);
+})();
