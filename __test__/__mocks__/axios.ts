24c0d0e1c2ed04b1e44471ad7d17523f26fb360a
shuhei.morioka <selmertsx@gmail.com>
2019-05-03 00:12:41 +0900

    axiosのmanual mockを作成した。レスポンスデータをどうするかは後で考える

--- __test__/__mocks__/axios.ts
+++ __test__/__mocks__/axios.ts
@@ -1,4 +1,16 @@
 // https://jestjs.io/docs/ja/manual-mocks
 // この手順に従ってManual Mockを行うことにする
-import axios from "axios";
 import { CountAPMHostsResponse } from "../mocked/DatadogResponse";
+
+const axios = jest.genMockFromModule("fs") as any;
+
+function get(url: string, config: any) {
+  return CountAPMHostsResponse;
+}
+
+function create(config: any) {
+  return { get };
+}
+
+axios.create = create;
+module.exports = axios;
