46242508834d86491b953566de5cb8b12fb05d63
shuhei.morioka <selmertsx@gmail.com>
2019-05-04 15:38:46 +0900

    axiosのmockに関して、所望するresponseを外部から渡すことができるようにした

--- __test__/DatadogClient.spec.ts
+++ __test__/DatadogClient.spec.ts
@@ -1,14 +1,25 @@
+import axios from "axios";
 import moment from "moment";
+import { CountAPMHostsResponse } from "../__test__/mocked/DatadogAPMResponse";
 import { DatadogClient } from "../src/DatadogClient";
 
 jest.mock("axios");
+
 const fromTime = moment({ years: 2018, months: 11, days: 18, hours: 9, minutes: 0, seconds: 0 }).format("x");
 const toTime = moment({ years: 2018, months: 11, days: 18, hours: 10, minutes: 0, seconds: 0 }).format("x");
+const client = new DatadogClient();
 
-describe("countHosts", () => {});
+xdescribe("countHosts", () => {
+  test("", () => {
+    const expected = [];
+    expected.push({ product: "product1" });
+    return expect(client.countHosts(fromTime, toTime)).resolves.toEqual(expected);
+  });
+});
 describe("countAPMHosts", () => {
   test("", () => {
-    const client = new DatadogClient();
+    (axios as any).__setMockResponse(CountAPMHostsResponse);
+
     const expected = new Map();
     expected.set("product1", new Map([[1556636400000, 2], [1556719200000, 2]]));
     expected.set("product2", new Map([[1556636400000, 1], [1556719200000, 1]]));
--- __test__/__mocks__/axios.ts
+++ __test__/__mocks__/axios.ts
@@ -1,11 +1,17 @@
 // https://jestjs.io/docs/ja/manual-mocks
 // この手順に従ってManual Mockを行うことにする
-import { CountAPMHostsResponse } from "../mocked/DatadogAPMResponse";
 
 const axios = jest.genMockFromModule("fs") as any;
+let mockedResponse = Object.create(null);
 
 function get(url: string, config: any) {
-  return CountAPMHostsResponse;
+  const result = mockedResponse;
+  mockedResponse = Object.create(null);
+  return result;
+}
+
+function __setMockResponse(response: any) {
+  mockedResponse = response;
 }
 
 function create(config: any) {
@@ -13,4 +19,5 @@ function create(config: any) {
 }
 
 axios.create = create;
+axios.__setMockResponse = __setMockResponse;
 module.exports = axios;
