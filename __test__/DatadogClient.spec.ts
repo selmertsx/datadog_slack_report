c112a3b5b40752bf3c57ab510edb63ca52233afa
shuhei.morioka <selmertsx@gmail.com>
2019-05-04 16:24:48 +0900

    test countHost methods in DatadogClient class

--- __test__/DatadogClient.spec.ts
+++ __test__/DatadogClient.spec.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import moment from "moment";
 import { CountAPMHostsResponse } from "../__test__/mocked/DatadogAPMResponse";
+import { CountSystemCPUResponse } from "../__test__/mocked/DatadogSystemCPUMetrics";
 import { DatadogClient } from "../src/DatadogClient";
 
 jest.mock("axios");
@@ -9,17 +10,21 @@ const fromTime = moment({ years: 2018, months: 11, days: 18, hours: 9, minutes: 
 const toTime = moment({ years: 2018, months: 11, days: 18, hours: 10, minutes: 0, seconds: 0 }).format("x");
 const client = new DatadogClient();
 
-xdescribe("countHosts", () => {
+describe("countHosts", () => {
   test("", () => {
+    (axios as any).__setMockResponse(CountSystemCPUResponse);
+
     const expected = [];
-    expected.push({ product: "product1" });
+    expected.push({ product: "product1", pointlists: new Map([[1556636400000, 3], [1556719200000, 3]]) });
+    expected.push({ product: "product2", pointlists: new Map([[1556636400000, 1], [1556719200000, 1]]) });
     return expect(client.countHosts(fromTime, toTime)).resolves.toEqual(expected);
   });
 });
+
 describe("countAPMHosts", () => {
   test("", () => {
+    // 外部からmockするResponseを指定することはできるようになったけれども、これはFactoryで設定できた方が良い気がする。
     (axios as any).__setMockResponse(CountAPMHostsResponse);
-
     const expected = new Map();
     expected.set("product1", new Map([[1556636400000, 2], [1556719200000, 2]]));
     expected.set("product2", new Map([[1556636400000, 1], [1556719200000, 1]]));
--- __test__/mocked/DatadogSystemCPUMetrics.ts
+++ __test__/mocked/DatadogSystemCPUMetrics.ts
@@ -1,32 +1,32 @@
 import { DatadogQueryResponse, SeriesMetrics } from "../../src/typings/datadog";
 
 const product1Metrics: SeriesMetrics = {
-  end: 1556722799000,
+  end: 1556719200000,
   attributes: {},
   metric: "system.cpu.user",
   interval: 3600,
-  start: 1556722799000,
+  start: 1556636400000,
   length: 24,
   query_index: 0,
   aggr: "count",
   scope: "product:product1",
-  pointlist: [[1556722799000, 3], [1556719200000, 3]],
+  pointlist: [[1556636400000, 3], [1556719200000, 3]],
   expression: "count:system.cpu.user{product:product1}.rollup(count, 3600)",
   unit: null,
   display_name: "system.cpu.user",
 };
 
 const product2Metrics: SeriesMetrics = {
-  end: 1556722799000,
+  end: 1556719200000,
   attributes: {},
   metric: "system.cpu.user",
   interval: 3600,
-  start: 1556722799000,
+  start: 1556636400000,
   length: 24,
   query_index: 0,
   aggr: "count",
   scope: "product:product2",
-  pointlist: [[1556722799000, 1], [1556719200000, 1]],
+  pointlist: [[1556636400000, 1], [1556719200000, 1]],
   expression: "count:system.cpu.user{product:product2}.rollup(count, 3600)",
   unit: null,
   display_name: "system.cpu.user",
