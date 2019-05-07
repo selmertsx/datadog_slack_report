994e630c122290a19951dcea38443e9818986460
shuhei.morioka <selmertsx@gmail.com>
2019-05-07 17:02:42 +0900

    [refactoring] IngraReportsMessage class

--- src/InfraReportsMessage.tsx
+++ src/InfraReportsMessage.tsx
@@ -19,14 +19,8 @@ export class InfraReportsMessage {
   }
 
   private monitoredTime(): string {
-    const fromTime = moment
-      .unix(parseInt(this.fromTime, 10))
-      .format("LLL")
-      .toString();
-    const toTime = moment
-      .unix(parseInt(this.toTime, 10))
-      .format("LLL")
-      .toString();
+    const fromTime = moment.unix(parseInt(this.fromTime, 10)).format("LLL").toString();
+    const toTime = moment.unix(parseInt(this.toTime, 10)).format("LLL").toString();
 
     return `${fromTime} ~ ${toTime}`;
   }
@@ -56,20 +50,24 @@ export class InfraReportsMessage {
 
   private outline(){
     const exceedHours = this.exceedHours();
-    return (exceedHours === 0) ? (
-      <Section>
-        <b>Datadog監視レポート</b> <br />
-        {this.monitoredTime()} <br />
-        上記の期間において予定を超過した数のホストは監視されませんでした
-      </Section>
-    ):(
-      <Section>
-        <b>Datadog監視レポート</b> <br />
-        {this.monitoredTime()} <br />
-        {exceedHours}時間分、予定した数のホストを超過して監視しました <br/>
-        内訳は下記のようになります。
-      </Section>
-    )
+    if (exceedHours === 0){
+      return (
+        <Section>
+          <b>Datadog監視レポート</b> <br />
+          {this.monitoredTime()} <br />
+          上記の期間において予定を超過した数のホストは監視されませんでした
+        </Section>
+      )
+    } else {
+      return (
+        <Section>
+          <b>Datadog監視レポート</b> <br />
+          {this.monitoredTime()} <br />
+          {exceedHours}時間分、予定した数のホストを超過して監視しました <br/>
+          内訳は下記のようになります。
+        </Section>
+      )
+    }
   }
 
   public body() {
