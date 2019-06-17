25575f8cfb4ad8606893451e1b365ee2c9755f29
shuhei.morioka <selmertsx@gmail.com>
2019-06-17 16:46:23 +0900

    DatadogMetricsFactoryをInfraReportsに組み込んだ

--- __test__/DatadogMetricsFactory.ts
+++ __test__/DatadogMetricsFactory.ts
@@ -7,4 +7,9 @@ export interface DatadogMetricsFactoryInput {
 }
 
 // @todo 期間を渡せば、1時間単位のpointListsを自動で作るようにしたい
-export function create(firstTime: string, lastTime: string, data: any) {}
+export function create(firstTime: string, lastTime: string, data: DatadogMetricsFactoryInput) {
+  return {
+    pointlists: new Map([[parseInt(firstTime, 10), data.maxInfraHosts], [parseInt(lastTime, 10), data.minInfraHosts]]),
+    product: data.name,
+  };
+}
--- __test__/InfraReports.spec.ts
+++ __test__/InfraReports.spec.ts
@@ -2,29 +2,29 @@ import moment from "moment";
 import { InfraReports } from "../src/InfraReports";
 import { DatadogHostMetrics, DatadogMonitoringPlan } from "../src/typings/datadog";
 import { ProductReport } from "../src/typings/products";
+import * as DatadogMetricsFactory from "./DatadogMetricsFactory";
 
 // tslint:disable: object-literal-sort-keys
 const firstTime = moment({ years: 2018, months: 11, days: 18, hours: 9, minutes: 0, seconds: 0 }).format("x");
 const lastTime = moment({ years: 2018, months: 11, days: 18, hours: 10, minutes: 0, seconds: 0 }).format("x");
-const sampleA = { maxInfraHosts: 40, minInfraHosts: 20, name: "sampleA", plannedInfraHosts: 30 };
-const sampleB = { maxInfraHosts: 30, minInfraHosts: 20, name: "sampleB", plannedInfraHosts: 30 };
+
+const sampleA: DatadogMetricsFactory.DatadogMetricsFactoryInput = {
+  maxInfraHosts: 40,
+  minInfraHosts: 20,
+  name: "sampleA",
+  plannedInfraHosts: 30,
+};
+
+const sampleB: DatadogMetricsFactory.DatadogMetricsFactoryInput = {
+  maxInfraHosts: 30,
+  minInfraHosts: 20,
+  name: "sampleB",
+  plannedInfraHosts: 30,
+};
 // tslint:enable: object-literal-sort-keys
 
-const sampleAMetrics = {
-  pointlists: new Map([
-    [parseInt(firstTime, 10), sampleA.maxInfraHosts],
-    [parseInt(lastTime, 10), sampleA.minInfraHosts],
-  ]),
-  product: sampleA.name,
-};
-
-const sampleBMetrics = {
-  pointlists: new Map([
-    [parseInt(firstTime, 10), sampleB.maxInfraHosts],
-    [parseInt(lastTime, 10), sampleB.minInfraHosts],
-  ]),
-  product: sampleB.name,
-};
+const sampleAMetrics = DatadogMetricsFactory.create(firstTime, lastTime, sampleA);
+const sampleBMetrics = DatadogMetricsFactory.create(firstTime, lastTime, sampleB);
 
 const sampleAMetricsUnderLimit = {
   pointlists: new Map([
